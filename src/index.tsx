b9402494b98048120d30c8828742b671db24d251
OOOytun <oytun@oakslab.com>
2023-01-18 23:29:22 +0300

    change index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-// import ReactDOM from 'react-dom';
-import { createRoot } from 'react-dom/client';
 
-const root = createRoot(document.getElementById('root')!);
-root.render(<App />);
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
