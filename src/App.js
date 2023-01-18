071d62bc81ddda50b13f1370b3df26d7b114ab06
Oytun <oytun@oakslab.com>
2023-01-18 12:47:27 +0300

    a

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload. meow
+          Edit <code>src/App.js</code> and save to reload. meowa
         </p>
         <a
           className="App-link"
