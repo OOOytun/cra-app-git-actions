471e02fded7e8d96fa8ead1835998247560aac2c
OOOytun <oytun@oakslab.com>
2023-01-18 23:27:44 +0300

    versioning changes

--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,7 @@
 import './index.css';
 import App from './App';
-import ReactDOM from 'react-dom';
+// import ReactDOM from 'react-dom';
+import { createRoot } from 'react-dom/client';
 
-const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
-);
+const root = createRoot(document.getElementById('root')!);
 root.render(<App />);
